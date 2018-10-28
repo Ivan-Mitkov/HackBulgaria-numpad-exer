e786978eb0ba06f772174f0f7a2688bd4bec22f3
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-10-28 21:02:15 +0200

    small changes

--- src/App.js
+++ src/App.js
@@ -6,40 +6,41 @@ import Grid from "./components/grid/Grid";
 class App extends Component {
   state = {
     data: data,
-    res: '',
-    light: false,
-    
+    res: "",
+    light: false
   };
   onOfLightHandler = () => {
-     this.setState({light : !this.state.light});
+    this.setState({ light: !this.state.light });
   };
-  onScreenHandler=(elem)=>{
-    if(this.state.light){
-      this.setState({res:this.state.res+" "+elem})
+  onScreenHandler = elem => {
+    if (this.state.light) {
+      this.setState({ res: this.state.res + " " + elem });
     }
-    
-  }
+  };
   render() {
     // console.log(this.state.data.keys);
     let lightCss = classes.smallDot;
     if (this.state.light) {
       lightCss += " " + classes.lightOn;
-    
     }
-    let onScreen='';
-    if(this.state.res){
-      onScreen=this.state.res
+    let onScreen = "";
+    if (this.state.res) {
+      onScreen = this.state.res;
     }
     return (
       <div className={classes.container}>
         <div className={classes.screen}>{onScreen}</div>
-        <div className={classes.light} >
+        <div className={classes.light}>
           <div className={classes.bigDot}>
             <span className={lightCss}> </span>
           </div>
           <div className={classes.num}>Num Lock</div>
         </div>
-        <Grid data={this.state.data.keys} click={this.onScreenHandler} numLock={this.onOfLightHandler}/>
+        <Grid
+          data={this.state.data.keys}
+          click={this.onScreenHandler}
+          numLock={this.onOfLightHandler}
+        />
       </div>
     );
   }
--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -4,22 +4,23 @@ import classes from "./Grid.module.css";
 // props.data props.click props.numpad
 const Grid = props => {
   let rows = [];
+  
   for (let i = 0; i < props.data.length; i++) {
+    let data=props.data[i].keyName;
     // console.log(props.data[i].keyName || 1);
-
     let row = (
       <div
         key={uuid4()}
         className={classes.item}
-        onClick={() => props.click(props.data[i].keyName)}
+        onClick={() => props.click(data)}
       >
-        {props.data[i].keyName}
+        {data}
       </div>
     );
-    if (props.data[i].keyName === "Lock") {
+    if (data === "Lock") {
       row = (
-        <div key={uuid4()} className={classes.item} onClick ={props.numLock}>
-          {props.data[i].keyName}
+        <div key={uuid4()} className={classes.item} onClick={props.numLock}>
+          {data}
         </div>
       );
     }
@@ -28,9 +29,9 @@ const Grid = props => {
         <div
           key={uuid4()}
           className={`${classes.item} ${classes.doubleHeight}`}
-          onClick={() => props.click(props.data[i].keyName)}
+          onClick={() => props.click(data)}
         >
-          {props.data[i].keyName}
+          {data}
         </div>
       );
     }
@@ -39,17 +40,14 @@ const Grid = props => {
         <div
           key={uuid4()}
           className={`${classes.item} ${classes.doubleWidth}`}
-          onClick={() => props.click(props.data[i].keyName)}
+          onClick={() => props.click(data)}
         >
-          {props.data[i].keyName}
+          {data}
         </div>
       );
     }
-
     rows.push(row);
   }
-
   return <div className={`${classes.container}`}>{rows}</div>;
 };
-
 export default Grid;
